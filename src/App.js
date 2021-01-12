cf5cb79d3b526aaae91d0bada0c6ab3329d2532a
Alexey Khisamutdinov <alexey-khisamutdinov@users.noreply.github.com>
2021-01-11 19:09:33 -0800

    Display different messages based on status

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import {getStatus} from './eventCountdown/plukService.js'
 import './App.css';
 
+function WaitForP(pStatus) {
+  return (
+    <header className="App-header">
+      <p>
+        Плюк будет {pStatus.startDate.toLocaleString("ru-ru", {timeZone: "Europe/Samara"})}
+      </p>
+      <p>
+        Ждать осталось {pStatus.daysLeft} {pStatus.ruDays}
+      </p>        
+      <p>
+        {pStatus.daysLeft.toString(2)}/bin | {pStatus.daysLeft.toString(16)}/hex
+      </p>        
+    </header>
+  )
+}
+
+function GoForP(pStatus) {
+  return (
+    <header className="App-header">
+      <p>
+        Плюк закончится {pStatus.endDate.toLocaleString("ru-ru", {timeZone: "Europe/Samara"})}
+      </p>
+      <p>
+        Ещё есть время! Собирай шмотки и бегом на электричку!
+      </p>         
+    </header>
+  )
+}
+
 function App() {
-  const pStatus = getStatus(new Date());
-  const message = "Next Pluk Date is " + pStatus.startDate ;
-  const message2 = "Starts in " +  pStatus.daysLeft + " days";
+  const today = new Date();
+  // today.setDate(26);
+  // today.setMonth(5);
+  const pStatus = getStatus(today);
+  const message = pStatus.status === "inProgress" ? <GoForP {...pStatus} /> : <WaitForP {...pStatus}/> 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          {message}
-        </p>
-        <p>
-          {message2}
-        </p>
-      </header>
+      {message}
     </div>
   );
 }
